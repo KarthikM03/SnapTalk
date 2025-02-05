9b2da8b370faf05314b304f539f32ab534870d5e
M karthik <95424179+KarthikM03@users.noreply.github.com>
2025-02-05 16:42:59 +0530

    Update static.js

--- Routers/static.js
+++ Routers/static.js
@@ -11,7 +11,7 @@ const path = require('path')
 //Profiles
 const ProfileStrorage = multer.diskStorage({
     destination: function (req, file, cb) {
-        return cb(null,'/Public/UserProfiles')
+        return cb(null,path.join(__dirname,'/Public/UserProfiles'))
     },
     filename: function (req, file, cb) {
         const filename = Date.now() + ".png"
@@ -34,7 +34,7 @@ const Profile = multer({
 //Posts
 const PostStrorage = multer.diskStorage({
     destination: function (req, file, cb) {
-        return cb(null, '/Public/Posts')
+        return cb(null, path.join(__dirname,'/Public/Posts'))
     },
     filename: function (req, file, cb) {
         const filename = Date.now() + ".png"
